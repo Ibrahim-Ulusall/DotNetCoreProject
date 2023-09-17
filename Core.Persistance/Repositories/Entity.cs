6931390d4d7dbaf2de1838a79d7d6ca605097d45
ibrahim <ibrahim@gmail.com>
2023-09-17 15:32:17 +0300

    Entity Oluşturuldu.

---
+++ Core.Persistance/Repositories/Entity.cs
@@ -0,0 +1,12 @@
+﻿namespace Core.Persistance.Repositories;
+public class Entity<TId>
+{
+    public TId Id { get; set; }
+    public DateTime CreatedDate { get; set; }
+    public DateTime? DeletedDate { get; set; }
+    public DateTime? UpdatedDate { get; set; }
+
+    public Entity() => Id = default;
+
+    public Entity(TId id) => Id = id;
+}
